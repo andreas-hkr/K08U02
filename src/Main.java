188c7e14eb9dd597c859f814215694f1fec6d585
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 14:27:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en metod som returnerar prisen inklusive moms
+        //   - Parameter 1: Priset utan moms
+        //   - Parameter 2: Momssatsen i procent
+
+
+    }
+}
