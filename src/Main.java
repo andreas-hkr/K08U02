ed1e509bf6c1fd99c5321542c277c81fd596e8ed
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 14:31:32 +0100

    Spelling mistake corrected

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Skapa en metod som returnerar prisen inklusive moms
+        // Skapa en metod som returnerar priset inklusive moms
         //   - Parameter 1: Priset utan moms
         //   - Parameter 2: Momssatsen i procent
 
